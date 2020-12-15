97f109ad2f6f2812f7b639c8a104b57b55c1f3d0
danmarrod <danmarrod@alum.us.es>
2020-12-15 12:23:17 +0100

    Added new contact - L06Step7

--- src/Contacts.js
+++ src/Contacts.js
@@ -1,33 +1,56 @@
 import React from 'react';
 import Contact from './Contact.js';
 import Alert from './Alert.js';
+import NewContact from './NewContact.js';
 
 class Contacts extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedContact: null
+            errorInfo: null,
+            contacts: this.props.contacts
         };
         this.handleEdit = this.handleEdit.bind(this);
         this.handeCloseError = this.handleCloseError.bind(this);
+        this.onAddContact = this.onAddContact.bind(this);
     }
 
     handleEdit(contact) {
         this.setState({
-            selectedContact: contact.name
+            errorInfo: contact.name
         });
     }
 
     handleCloseError() {
         this.setState({
-            selectedContact: null
+            errorInfo: null
         });
+    }
+
+    onAddContact(contact) {
+        // previous state cannot be change, its immutable, then we need copy previous content and add the new contact
+        this.setState(prevState => {
+            const contacts = prevState.contacts;
+            // check there is not two equals names, we ara using 'name' as a primary key
+            if (!contacts.find (c => c.name === contact.name)) {
+                return({
+                    //create a new array with the last content of contacts and add new contact
+                    contacts: [...prevState.contacts, contact]
+                });
+            }
+
+            return({
+                errorInfo: 'Contact already exists'
+            });
+           
+        });
+
     }
 
     render() {
         return (
             <div>
-                <Alert message={this.state.selectedContact} onClose={this.handeCloseError} />
+                <Alert message={this.state.errorInfo} onClose={this.handeCloseError} />
                 <table class="table" >
                     <thead>
                         <tr>
@@ -36,7 +59,8 @@ class Contacts extends React.Component {
                             <th>&nbsp;</th>
                         </tr>
                     </thead>
-                    {this.props.contacts.map((contact) =>
+                    <NewContact onAddContact={this.onAddContact}></NewContact>
+                    {this.state.contacts.map((contact) =>
                         <Contact contact={contact} onEdit={this.handleEdit} />
                     )}
                 </table >
